b463b94cc419859cc93475eb2a7e28dfc3031d99
AdrianFagilde <adrianfagilde@gmail.com>
2026-09-21 00:44:40 -0400

    style: add Chakra Petch + Share Tech Mono typography and subtle HUD card frames

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+
+import '@fontsource/chakra-petch/latin-400.css'
+import '@fontsource/chakra-petch/latin-500.css'
+import '@fontsource/chakra-petch/latin-600.css'
+import '@fontsource/chakra-petch/latin-700.css'
+import '@fontsource/share-tech-mono/latin-400.css'
+
 import { AppProvider } from './context/AppContext'
 
 import App from './App'
